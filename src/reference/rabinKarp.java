e77320c212786287d555a5af91832f79eb4c9926
YouZhou <zhouyou0528@sina.com>
2017-10-30 17:20:36 -0400

    rabinKarp

--- src/reference/rabinKarp.java
+++ src/reference/rabinKarp.java
@@ -1,3 +1,5 @@
+package reference;
+
 /******************************************************************************
  *  Compilation:  javac RabinKarp.java
  *  Execution:    java RabinKarp pat txt
--- src/reference/testRabinKarp.java
+++ src/reference/testRabinKarp.java
@@ -1,3 +1,5 @@
+package reference;
+
 
 public class testRabinKarp {
 	
